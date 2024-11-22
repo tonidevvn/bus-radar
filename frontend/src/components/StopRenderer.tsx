0d4646e2e139111478a40e83fc4939c6513d5dd3
Toni Pham <tonidevvn@gmail.com>
2024-11-22 00:14:36 -0500

    ui fix icon size on map zoom events

--- frontend/src/components/StopRenderer.tsx
+++ frontend/src/components/StopRenderer.tsx
@@ -1,73 +1,87 @@
-import { RootState } from '@reduxjs/toolkit/query'
-import L from 'leaflet'
-import { useEffect, useState } from 'react'
-import { Marker, Popup } from 'react-leaflet'
-import { useDispatch, useSelector } from 'react-redux'
-import { GET_STOPS } from '../api/bus_api'
-import { setLoading } from '../store/slices/appSlice'
-import { setPickStop } from '../store/slices/busSlice'
-import { AppDispatch } from '../store/store'
-import { Stop } from '../types/type'
-import StopSchedule from './StopSchedule'
+import { RootState } from "@reduxjs/toolkit/query";
+import L from "leaflet";
+import { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import { useDispatch, useSelector } from "react-redux";
+import { GET_STOPS } from "../api/bus_api";
+import { setLoading } from "../store/slices/appSlice";
+import { setPickStop } from "../store/slices/busSlice";
+import { AppDispatch } from "../store/store";
+import { Stop } from "../types/type";
+import StopSchedule from "./StopSchedule";
 
-const DefaultIcon = L.icon({
-    iconUrl: '/circle.png',
-    iconSize: [20, 20], // Size of the circle including the border (20px + 2px border on each side)
-    iconAnchor: [12, 12], // Anchor to align the circle properly on the map
-    popupAnchor: [0, -12], // Adjust popup location relative to the icon
-})
-
-const HighlightIcon = L.icon({
-    iconUrl: '/highlight_circle.png', // Replace with an image to indicate the highlighted stop
-    iconSize: [25, 25], // Slightly larger size for emphasis
-    iconAnchor: [12, 12],
-    popupAnchor: [0, -12],
-})
+const StopIcon = (size: number, iconUrl: string) =>
+  L.icon({
+    iconUrl,
+    iconSize: [size, size],
+    iconAnchor: [size / 2, size / 2],
+    popupAnchor: [0, -size / 2],
+  });
 
 const StopsRenderer = () => {
-    const [stops, setStops] = useState<Stop[]>([])
-    const { patternIDs, pickStop } = useSelector(
-        (state: RootState) => state.bus
-    )
-    const dispatch = useDispatch<AppDispatch>()
-    useEffect(() => {
-        if (!patternIDs) {
-            return
-        }
-        GET_STOPS(patternIDs)
-            .then((data) => {
-                setStops(data)
-            })
-            .finally(() => {
-                dispatch(setLoading(false))
-            })
-    }, [patternIDs, dispatch])
+  const [stops, setStops] = useState<Stop[]>([]);
+  const { patternIDs, pickStop } = useSelector((state: RootState) => state.bus);
+  const [iconSize, setIconSize] = useState(20); // Default size for stops
+  const defaultZoom = 13;
+  const [zoom, setZoom] = useState(defaultZoom); // Default zoom level
+  const map = useMap();
 
-    return (
-        <>
-            {stops.map((stop) => (
-                <Marker
-                    key={stop.stopID}
-                    position={[stop.latitude, stop.longitude]}
-                    icon={
-                        pickStop?.stopID === stop.stopID
-                            ? HighlightIcon
-                            : DefaultIcon
-                    }
-                    eventHandlers={{
-                        click: () => {
-                            dispatch(setPickStop(stop))
-                        },
-                    }}
-                >
-                    <Popup>
-                        {/* <StopInfo stop={stop} /> */}
-                        <StopSchedule />
-                    </Popup>
-                </Marker>
-            ))}
-        </>
-    )
-}
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    if (!patternIDs) {
+      return;
+    }
+    GET_STOPS(patternIDs)
+      .then((data) => {
+        setStops(data);
+      })
+      .finally(() => {
+        dispatch(setLoading(false));
+      });
+  }, [patternIDs, dispatch]);
 
-export default StopsRenderer
+  useEffect(() => {
+    // Update icon size based on zoom level
+    const updateIconSize = () => {
+      const zoom = map.getZoom();
+      const size = zoom < defaultZoom ? 8 : Math.max(8, zoom * 1.15); // Reduce the size when zooming out, capped at 8px minimum
+      setIconSize(size);
+      setZoom(zoom);
+    };
+
+    map.on("zoomend", updateIconSize); // Listen for zoom changes
+    updateIconSize(); // Set size initially
+
+    return () => {
+      map.off("zoomend", updateIconSize); // Cleanup listener
+    };
+  }, [map]);
+
+  return (
+    <>
+      {stops.map((stop) => (
+        <Marker
+          key={`${stop.stopID}-${zoom}`} // Key includes zoom to force re-render
+          position={[stop.latitude, stop.longitude]}
+          icon={
+            pickStop?.stopID === stop.stopID
+              ? StopIcon(iconSize + 5, "/highlight_circle.png") // Highlight icon
+              : StopIcon(iconSize, "/circle.png") // Default icon
+          }
+          eventHandlers={{
+            click: () => {
+              dispatch(setPickStop(stop));
+            },
+          }}
+        >
+          <Popup>
+            {/* <StopInfo stop={stop} /> */}
+            <StopSchedule />
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  );
+};
+
+export default StopsRenderer;
--- frontend/src/components/VehicleRenderer.tsx
+++ frontend/src/components/VehicleRenderer.tsx
@@ -1,54 +1,78 @@
-import L from 'leaflet'
-import { useEffect, useState } from 'react'
-import { Marker, Popup } from 'react-leaflet'
-import { useSelector } from 'react-redux'
-import { GET_VEHICLE_STATUS } from '../api/bus_api'
-import { VehicleStatus } from '../types/type'
-import { getCurrentDayOfWeek } from '../utils/Date'
-import VehicleStatusInfo from './VehicleStatusInfo'
+import L from "leaflet";
+import { useEffect, useState } from "react";
+import { Marker, Popup, useMap } from "react-leaflet";
+import { useSelector } from "react-redux";
+import { GET_VEHICLE_STATUS } from "../api/bus_api";
+import { VehicleStatus } from "../types/type";
+import { getCurrentDayOfWeek } from "../utils/Date";
+import VehicleStatusInfo from "./VehicleStatusInfo";
 
-const BusIcon = L.icon({
-    iconUrl: 'https://windsor.mapstrat.com/images/vehicle.png', // Replace with the actual path to your bus icon image
-    iconSize: [20, 20], // Adjust the size of the icon as needed
-    iconAnchor: [12, 12], // Adjust anchor to align the icon properly on the map
-    popupAnchor: [1, -30], // Adjust popup location relative to the icon
-})
+const BusIcon = (size: number) =>
+  L.icon({
+    iconUrl: "/vehicle.png", // Replace with your bus icon
+    iconSize: [size, size],
+    iconAnchor: [size / 2, size / 2],
+    popupAnchor: [1, -size / 2],
+  });
 
 const VehicleRenderer = () => {
-    const [vehicles, setVehicles] = useState<VehicleStatus[]>([])
-    const { routeID, patternIDs } = useSelector((state: RootState) => state.bus)
-    useEffect(() => {
-        const fetchData = () => {
-            if (!patternIDs) {
-                return
-            }
-            GET_VEHICLE_STATUS(patternIDs, routeID, getCurrentDayOfWeek()).then(
-                (data) => {
-                    setVehicles(data)
-                }
-            )
+  const [vehicles, setVehicles] = useState<VehicleStatus[]>([]);
+  const { routeID, patternIDs } = useSelector((state: RootState) => state.bus);
+  const [iconSize, setIconSize] = useState(50); // Default size for vehicles
+  const defaultZoom = 13;
+  const [zoom, setZoom] = useState(defaultZoom); // Default zoom level
+  const map = useMap();
+
+  useEffect(() => {
+    const fetchData = () => {
+      if (!patternIDs) {
+        return;
+      }
+      GET_VEHICLE_STATUS(patternIDs, routeID, getCurrentDayOfWeek()).then(
+        (data) => {
+          setVehicles(data);
         }
+      );
+    };
 
-        const interval = setInterval(fetchData, 3000)
+    const interval = setInterval(fetchData, 3000);
 
-        return () => clearInterval(interval)
-    }, [patternIDs])
+    return () => clearInterval(interval);
+  }, [patternIDs]);
 
-    return (
-        <>
-            {vehicles.map((vehicle) => (
-                <Marker
-                    key={vehicle.vehicleId}
-                    position={[vehicle.lat, vehicle.lng]}
-                    icon={BusIcon}
-                >
-                    <Popup>
-                        <VehicleStatusInfo vehicle={vehicle} />
-                    </Popup>
-                </Marker>
-            ))}
-        </>
-    )
-}
+  useEffect(() => {
+    // Update icon size based on zoom level
+    const updateIconSize = () => {
+      const zoom = map.getZoom();
+      const size = zoom > defaultZoom ? 50 : zoom < 10 ? 25 : 35; // Reduce the size when zooming out
+      setIconSize(size);
+      setZoom(zoom);
+    };
 
-export default VehicleRenderer
+    map.on("zoomend", updateIconSize); // Listen for zoom changes
+    updateIconSize(); // Set size initially
+
+    return () => {
+      map.off("zoomend", updateIconSize); // Cleanup listener
+    };
+  }, [map]);
+
+  return (
+    <>
+      {vehicles.map((vehicle) => (
+        <Marker
+          key={`${vehicle.vehicleId}-${zoom}`} // Key includes zoom to force re-render
+          position={[vehicle.lat, vehicle.lng]}
+          icon={BusIcon(iconSize)}
+          zIndexOffset={9999}
+        >
+          <Popup>
+            <VehicleStatusInfo vehicle={vehicle} />
+          </Popup>
+        </Marker>
+      ))}
+    </>
+  );
+};
+
+export default VehicleRenderer;
